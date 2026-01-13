858e3df9ac897a57ffb53cbc54e57c4ec7a22ac9
Spookybuddy <93450263+Spookybuddy@users.noreply.github.com>
2026-01-13 02:50:01 -0500

    Deterministic RNG
    Working on perfecting the generation:
    Made the generation seeded, with set attempts and a checklist to prevent checking the same tile against the same door multiple times. Working on additional path connections between remaining doors

--- Assets/Scripts/_Scriptables/Dungeon.cs
+++ Assets/Scripts/_Scriptables/Dungeon.cs
@@ -7,15 +7,25 @@ public class Dungeon : ScriptableObject
     [Tooltip("The Tiles that can spawn")]
     public TileWithWeight[] tileset;
     [Tooltip("Sum of the tiles' weights. Can be calculated via the Context Menu (three dots top right)")]
-    public uint weightSummation = 0;
+    public int weightSummation = 0;
     [Tooltip("The target total meterage of spawned Tiles")]
-    public uint targetSurfaceArea;
+    [Range(0, 100000)]
+    public int targetSurfaceArea;
     [Tooltip("Min variation added to tile rotation")]
-    [Range(0f, 90f)]
-    public float minRotationVariation = 15;
+    [Range(-128, 127)]
+    public sbyte minRotationVariation = 15;
     [Tooltip("Max variation added to tile rotation")]
-    [Range(0f, 120f)]
-    public float maxRotationVariation = 45;
+    [Range(-128, 127)]
+    public sbyte maxRotationVariation = 60;
+    [Header("Connections")]
+    [Tooltip("Randomly connect remaining doors after generating")]
+    public bool moreConnections = true;
+    [Tooltip("Connections with distance < (average spawn spacing * this) will be valid connections")]
+    [Range(1f, 10f)]
+    public float distanceMultiplier = 2.2f;
+    [Tooltip("Any dot product between doors < this will be valid connections")]
+    [Range(-1f, 1)]
+    public float dotLimit = -0.5f;
 
     [ContextMenu("Calculate Weight Sum")]
     public void SumWeights()
